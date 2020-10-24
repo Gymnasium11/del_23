5f1b4313465f31d735605cede9a1016bb5617d1e
Павел Яковлев <pavlik.yakovlev.2004@mail.ru>
2020-10-24 22:37:07 +0700

    i have commited

--- test.py
+++ test.py
@@ -3,16 +3,20 @@ def ras(x, y):
 
 
 def dev(x, y):
-    return x/y
+    return x / y
 
 
 def add(x, y):
+    fu = lambda z: z+5
+    print('hello')
+    x, y = divmod(x, y)
+    # this function do somethink
     return x + y
 
 
-def mult(x,y):
-    return x*y
-    
-    
+def mult(x, y):
+    return x * y
+
+
 def main():
     pass
